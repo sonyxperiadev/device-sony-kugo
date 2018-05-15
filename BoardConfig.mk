6a57e6f8b7c2c7fc34ce33f535e4f7bff33afb33
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-15 09:31:27 +0200

    Revert "remove deprecated PRODUCT_PLATFORM"
    This reverts commit d60442338970d0795a6a0486f911d326b726db1b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,9 @@ include device/sony/loire/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := loire
 
+# Platform
+PRODUCT_PLATFORM := loire
+
 BOARD_KERNEL_CMDLINE += androidboot.hardware=kugo
 
 # Partition information
